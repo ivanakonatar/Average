2e3d76119d5f7d8b5a0ad14d7ce8bbf5ae933ba0
Ivana Konatar <ivana.konatar@udg.edu.me>
2018-02-15 18:51:01 +0100

    treci

--- zadatak.py
+++ zadatak.py
@@ -8,6 +8,6 @@ def main():
     score1, score2=eval(input("Enter two scores separated bz a comma:"))
     average = (score1 + score2) / 2
 
-    print("Prosjeci su", average)
+    print("Prosjeci su test", average)
 
 main()
